fc15627b2ff35cd172c960f2805cebebac8d817a
maciej-jonczyk <56517984+maciej-jonczyk@users.noreply.github.com>
2023-08-30 10:49:42 +0200

    Localization of counts4r.sh

--- counts4r.sh
+++ counts4r.sh
@@ -1,62 +1,62 @@
-# przed każdą sesją
+# before each session
 export LC_ALL=C
-# plik po featureCounts - OSOBNY DLA DOSWIADCZENIA
+# file after featureCounts - SEPARATE FOR EXPERIMENT
 tail -n +2 counts-230420.txt > x
 head -n1 x | tr '\t' '\n' | cat -n
-# separatorem jest TAB
+# TAB is a field separator
 cut --complement -f2-6 -d"     " x > x2
 
 
-# plik z identyfikacją prób - OSOBNY DLA DOSWIADCZENIA
+# file with sample identification - SEPARATE FOR EXPERIMENT
 less proby-labels
-# wycięcie dwóch części id rozdzielanych kropką, żeby zrobić terminy po cyframi kolei
-# pierwotny separator to TAB
+# cut two parts of id separated by dot, to make sorted sampling time variable
+# TAB is a separator
 cut -f5 -d"    " proby-labels | cut -f1-2 -d"." > x
-# ręczne dodanie cyfr i eksport do csv z TAB jako separatorem
+# MANUAL addition of numbers and export to TAB-separated csv
 libreoffice --calc x
-# scalenie liczb do id
+# merging numbers and id
 tr '\t' '.' < x.csv > x3
-# dodanie nazwy kolumny z id i transpozycja
+# adding column name with id and transposition
 cat <(echo Geneid) x3 | tr '\n' '\t' > x4
-# ręczne usunięcie TABa z końca i przejście do nowego wiersza
-# usunięcie oryginalnego nagłówka z counts
+# MANUAL deleting TAB from end of line and moving to new line
+# removal of original header from counts
 tail -n +2 x2 > x5
-# dodanie nowego nagłówka
+# adding new header
 cat x4 x5 > counts4r
 
-# plik z opisem eksperymentu - OSOBNY DLA DOSWIADCZENIA 
-# dodanie zmodyfoikowanej kolumny z próbami
+# file with experiment description - SEPARATE FOR EXPERIMENT 
+# adding modyfied column with samples
 paste x3 proby-labels > x
-# terminy
+# sampling time
 cut -f3 -d"." x3 > x2
-# połączenie
+# sticking together
 paste x x2 > x3
-# nagłówek zgodny z kolumnami
+# header congruent with columns
 echo id litera tk ln dl-time dl-nazwa time | tr ' ' '\t' > x2
 cat x2 x3 > samples4r
 
-############ tego nie robić #############
-# zakresy, NIE z wyniku zliczeń, może zawierać wiele zakresów dla jednego genu
+############ I don't do that #############
+# ranges, NOT from count result, it may have multiple ranges per gene
 tail -n +2 counts-230420.txt | cut -f1-6 -d"  " > table4ranges
 ##########################################
 
-# Ponizsze WSPOLNE DLA EKSPERYMENTOW DLA GENOMU W DANEJ WERSJI
-# Kolejnośc w zliczeniach zgodna z tym bo one korzystały z tego GTF
+# Code and data below are THE SAME FOR EXPERIMENTS FOR GENOME IN A GIVEN VERSION
+# Order in counts congruent with this - they used the same GTF
 
-# zakresy z GTF
-# zakresy bez ID, można bez obliczania długości - i tak nie zgodzi się z width z IRanges
+# ranges from GTF
+# ranges without ID, it may be done without length calculation - either way it will be not congruent with width from IRanges
 awk -v FS="\t" -v OFS=" " '$3=="gene"{print $1,$4,$5,$7,$5-$4}'  ~/NAMv5/Zea_mays.Zm-B73-REFERENCE-NAM-5.0.55.chr.gtf > x
 # ID
 awk -v FS="\t" -v OFS=" " '$3=="gene"'  ~/NAMv5/Zea_mays.Zm-B73-REFERENCE-NAM-5.0.55.chr.gtf | cut -f2 -d'"' > x2
 paste -d" " x2 x > x3
-# nagłówek
+# header
 echo names seqnames start end strand length > x4
 cat x4 x3 > geny4ranges
 
-# długości chromosomów - wszystkich
+# chromosome lengths - for all
 zgrep -F "sequence-region"  ~/NAMv5/Zea_mays.Zm-B73-REFERENCE-NAM-5.0.55.gff3.gz | cut -f2,4 -d"     " > x
 tr -s " " < x | cut -f2,4 -d" " > x2
 sed '/^[0-9]/s/^/chr/' x2 > dlchrom
-# wybór tylko tych w zliczeniach
+# selecting only those present in counts
 cut -f2 -d"   " counts-230420.txt | tail -n +3 | cut -f1 -d";" | sort -u > counts.chrom
 grep -Fwf counts.chrom dlchrom > counts.dlchrom
